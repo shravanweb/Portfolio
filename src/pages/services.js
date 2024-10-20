4120861ecc21bd5a88df34f5cc2d7d3ed8b7a6a6
shravan <shravan@nityainc.com>
2024-10-20 20:38:56 +0530

    updated

--- src/pages/services.js
+++ src/pages/services.js
@@ -11,12 +11,12 @@ const services = [
     {
         title: 'Logo Design',
         description: 'Crafting memorable and impactful logos that represent your brand identity and resonate with your audience.',
-        imageUrl: 'https://images.unsplash.com/photo-1533750349088-cd871a92f312', // Replace with your image link
+        imageUrl: 'https://plus.unsplash.com/premium_photo-1661770132071-026114fffb61?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D.', // Replace with your image link
     },
     {
         title: 'Video Design',
         description: 'Producing high-quality videos that tell your story and captivate your audience, from concept to final cut.',
-        imageUrl: 'https://images.unsplash.com/photo-1559423125-c08d7d96d649', // Replace with your image link
+        imageUrl: 'https://images.unsplash.com/photo-1536240478700-b869070f9279?q=80&w=1900&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', // Replace with your imagelink
     },
 ];
 
@@ -24,22 +24,43 @@ const ServicesSection = () => {
     const { darkMode } = useThemeToggle(); // Access darkMode state
 
     return (
-        <Box sx={{
-            py: 10, backgroundColor: darkMode ? '#121212' : '#f3f4f6', color: darkMode ? '#fff' : '#333',  // Dark mode background color
- }}>
-            <Container maxWidth="lg">
-                <Typography variant="h3" align="center" gutterBottom sx={{ fontWeight: 'bold', marginBottom: 6 }}>
-                    Our Services
-                    
-                </Typography>
-                
+        <Box
+            sx={{
+                py: 10,
+                backgroundColor: darkMode ? '#121212' : '#f3f4f6',
+                color: darkMode ? '#fff' : '#333', // Dark mode background color
+            }}
+        >
+            <div maxWidth="lg" style={{padding:"50px"}}>
+                {/* Heading with horizontal line */}
+                <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-start', mb: 6 }}>
+                    <Typography
+                        variant="h3"
+                        align="left"
+                        sx={{
+                            fontWeight: 'bold',
+                            marginRight: '20px', // Space between heading and line
+                            color: darkMode ? '#fff' : '#333',
+                            fontSize:"40px"
+                        }}
+                    >
+                        Services
+                    </Typography>
+                    <Box sx={{ flexGrow: 1, height: '2px', backgroundColor: darkMode ? '#fff' : '#333' }} />
+                </Box>
+
                 <Grid container spacing={4}>
                     {services.map((service, index) => (
                         <Grid item xs={12} sm={6} md={4} key={index}>
-                            <Card sx={{
-                                height: '100%', boxShadow: 3, borderRadius: '16px',    backgroundColor: darkMode ? '#1f1f1f' : '#ffffff',
-                                color: darkMode ? '#e0e0e0' : '#333',
-}}>
+                            <Card
+                                sx={{
+                                    height: '100%',
+                                    boxShadow: 3,
+                                    borderRadius: '16px',
+                                    backgroundColor: darkMode ? '#1f1f1f' : '#ffffff',
+                                    color: darkMode ? '#e0e0e0' : '#333',
+                                }}
+                            >
                                 <CardMedia
                                     component="img"
                                     height="200"
@@ -47,14 +68,26 @@ const ServicesSection = () => {
                                     alt={service.title}
                                 />
                                 <CardContent>
-                                    <Typography variant="h5" component="div" sx={{ fontWeight: 'bold', marginBottom: 2, color: darkMode ? '#ffcc00' : '#007fff', }}>
+                                    <Typography
+                                        variant="h5"
+                                        component="div"
+                                        sx={{
+                                            fontWeight: 'bold',
+                                            marginBottom: 2,
+                                            color: darkMode ? '#ffcc00' : '#007fff',
+                                        }}
+                                    >
                                         {service.title}
                                     </Typography>
-                                    <Typography variant="body1" color="textSecondary" sx={{
-                                        color: darkMode ? '#e0e0e0' : '#555',
-                                        lineHeight: 1.8,
-                                        mb: 3,
-                                    }}>
+                                    <Typography
+                                        variant="body1"
+                                        color="textSecondary"
+                                        sx={{
+                                            color: darkMode ? '#e0e0e0' : '#555',
+                                            lineHeight: 1.8,
+                                            mb: 3,
+                                        }}
+                                    >
                                         {service.description}
                                     </Typography>
                                 </CardContent>
@@ -62,7 +95,7 @@ const ServicesSection = () => {
                         </Grid>
                     ))}
                 </Grid>
-            </Container>
+            </div>
         </Box>
     );
 };
--- src/pages/skills.js
+++ src/pages/skills.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Box, Typography } from '@mui/material';
 import { useThemeToggle } from '../app/ThemeToggleProvider'; // Import useThemeToggle hook
 
-
 const SkillsSection = () => {
     const { darkMode } = useThemeToggle(); // Access darkMode state
 
@@ -15,34 +14,35 @@ const SkillsSection = () => {
         { name: 'GitHub', logo: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg' },
         { name: 'MongoDB', logo: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg' },
         { name: 'Photoshop', logo: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/photoshop/photoshop-plain.svg' },
-        { name: 'Adobe XD', logo: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/adobe XD/adobe XD-plain.svg' },
+        { name: 'Adobe XD', logo: 'https://www.logo.wine/a/logo/Adobe_XD/Adobe_XD-Logo.wine.svg' },
         { name: 'WordPress', logo: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/wordpress/wordpress-original.svg' },
     ];
 
     return (
         <Box
             sx={{
-                padding: '70px',
-                textAlign: 'center',
                 padding: '50px',
+                textAlign: 'center',
                 backgroundColor: darkMode ? '#1c1c1c' : '#f9f9f9', // Dark mode background
                 color: darkMode ? '#fff' : '#333', // Dark mode text color
             }}
         >
-            <Typography
-                variant="h3"
-                component="h2"
-                sx={{
-                    mb: 4,
-                    fontWeight: 'bold',
-                    color: '#333',
-                    padding: '50px',
-                  
-                    color: darkMode ? '#fff' : '#333', // Dark mode text color
-                }}
-            >
-                My Skills
-            </Typography>
+            {/* Heading with horizontal line */}
+            <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-start', mb: 4 }}>
+                <Typography
+                    variant="h3"
+                    component="h2"
+                    sx={{
+                        fontWeight: 'bold',
+                        color: darkMode ? '#fff' : '#333',
+                        marginRight: '20px', // Space between heading and line
+                        fontSize:"40px"
+                    }}
+                >
+                    My Skills
+                </Typography>
+                <Box sx={{ flexGrow: 1, height: '2px', backgroundColor: darkMode ? '#fff' : '#333' }} />
+            </Box>
 
             {/* Horizontal Scrollable Container */}
             <Box
@@ -63,7 +63,6 @@ const SkillsSection = () => {
                         sx={{
                             display: 'inline-block',
                             padding: '10px',
-                          
                             borderRadius: '10px',
                             boxShadow: '0 4px 10px rgba(0, 0, 0, 0.1)',
                             textAlign: 'center',
